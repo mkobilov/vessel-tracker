833cd88cb0415028c7de8b35483e5422406680ad
mkobilov <kobilovm@gmail.com>
2019-11-25 00:48:15 +0300

    testbranch changes
    test

--- src/main/java/com/vts/vtsserver/repository/StationaryObjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vts.vtsserver.repository;
-
-import com.vts.vtsserver.model.StationaryObject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationaryObjectRepository extends JpaRepository<StationaryObject, Long> {
-}
--- src/main/java/com/vts/vtsserver/web/rest/dto/XY.java
+++
@@ -1,9 +0,0 @@
-package com.vts.vtsserver.web.rest.dto;
-
-import lombok.Data;
-
-@Data
-public class XY {
-    private Long x;
-    private Long y;
-}
